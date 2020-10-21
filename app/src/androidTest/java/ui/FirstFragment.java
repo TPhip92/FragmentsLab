e80a0b55a35edce6a74931f0af7ff3d9655c845c
TPhip92 <63381154+TPhip92@users.noreply.github.com>
2020-10-20 19:51:38 -0700

    Initial commit

---
+++ app/src/androidTest/java/ui/FirstFragment.java
@@ -0,0 +1,24 @@
+package ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.uwnetid.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+}
